c85fd1e2447d5cd6fb572aea59887cd216bfaebd
sogekingdabest <36851786+sogekingdabest@users.noreply.github.com>
2023-05-21 02:36:43 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,3 +58,9 @@ tasks.withType<JavaCompile>() {
 tasks.withType<Javadoc>() {
     options.encoding = "UTF-8"
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "es.Application"
+    }
+}
