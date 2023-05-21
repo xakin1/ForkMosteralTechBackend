5c79619c80d9e122bd65c1967de76250f1b54258
sogekingdabest <36851786+sogekingdabest@users.noreply.github.com>
2023-05-21 02:27:35 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,9 +58,3 @@ tasks.withType<JavaCompile>() {
 tasks.withType<Javadoc>() {
     options.encoding = "UTF-8"
 }
-
-tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-    manifest {
-        attributes["Main-Class"] = "es.Application"
-    }
-}
