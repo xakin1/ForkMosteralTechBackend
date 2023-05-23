694c42443dd20c1ac15354d1a076bf0673d2425a
xaquín <xaquin.perez1@udc.es>
2023-05-23 16:00:45 +0200

    add images to productsDtos

--- src/main/java/es/model/service/dto/ProductDTO.java
+++ src/main/java/es/model/service/dto/ProductDTO.java
@@ -1,109 +1,120 @@
 package es.model.service.dto;
 
+import java.util.List;
+
 import es.model.domain.Product;
+import es.model.domain.ProductImage;
 import es.model.domain.State;
 import es.model.repository.ProductRepository.ProductProjection;
 
 public class ProductDTO {
 
-  private Long id;
-  private String name;
-  private String description;
-  private Double price;
-  private ProductOwner productOwner;
-  private State state;
-  private boolean isFavourite;
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private ProductOwner productOwner;
+	private List<ProductImage> productImages;
+	private State state;
+	private boolean isFavourite;
 
-  public ProductDTO() {}
+	public ProductDTO() {
+	}
 
-  public ProductDTO(ProductProjection product) {
-    this(product.getProduct());
-    this.isFavourite = product.getIsFavourite();
-  }
+	public ProductDTO(ProductProjection product) {
+		this(product.getProduct());
+		this.isFavourite = product.getIsFavourite();
+	}
 
-  public ProductDTO(Product product) {
-    this.id = product.getId();
-    this.name = product.getName();
-    this.description = product.getDescription();
-    this.state = product.getState();
-    this.price = product.getPrice();
-    if (product.getOwner() != null)
-      this.productOwner =
-          new ProductOwner(
-              product.getOwner().getId(),
-              product.getOwner().getName(),
-              product.getOwner().getSurname());
-  }
+	public ProductDTO(Product product) {
+		this.id = product.getId();
+		this.name = product.getName();
+		this.description = product.getDescription();
+		this.state = product.getState();
+		this.price = product.getPrice();
+		this.productImages = product.getImages();
+		if (product.getOwner() != null)
+			this.productOwner = new ProductOwner(product.getOwner().getId(), product.getOwner().getName(),
+					product.getOwner().getSurname());
+	}
 
-  public ProductDTO(Product product, boolean isFavourite) {
-    this(product);
-    this.isFavourite = isFavourite;
-  }
+	public ProductDTO(Product product, boolean isFavourite) {
+		this(product);
+		this.isFavourite = isFavourite;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public String getDescription() {
+		return description;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  public State getState() {
-    return state;
-  }
+	public State getState() {
+		return state;
+	}
 
-  public void setState(State state) {
-    this.state = state;
-  }
+	public void setState(State state) {
+		this.state = state;
+	}
 
-  public Double getPrice() {
-    return price;
-  }
+	public Double getPrice() {
+		return price;
+	}
 
-  public void setPrice(Double price) {
-    this.price = price;
-  }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-  public boolean isFavourite() {
-    return isFavourite;
-  }
+	public boolean isFavourite() {
+		return isFavourite;
+	}
 
-  public void setFavourite(boolean isFavourite) {
-    this.isFavourite = isFavourite;
-  }
+	public void setFavourite(boolean isFavourite) {
+		this.isFavourite = isFavourite;
+	}
 
-  public ProductOwner getProductOwner() {
-    return productOwner;
-  }
+	public ProductOwner getProductOwner() {
+		return productOwner;
+	}
 
-  public void setProductOwner(ProductOwner productOwner) {
-    this.productOwner = productOwner;
-  }
+	public void setProductOwner(ProductOwner productOwner) {
+		this.productOwner = productOwner;
+	}
 
-  public Product toProduct() {
-    Product product = new Product();
-    product.setId(this.getId());
-    product.setName(this.getName());
-    product.setDescription(this.getDescription());
-    product.setState(this.getState());
-    product.setPrice(this.getPrice());
+	public List<ProductImage> getProductImages() {
+		return productImages;
+	}
 
-    return product;
-  }
+	public void setProductImages(List<ProductImage> productImages) {
+		this.productImages = productImages;
+	}
+
+	public Product toProduct() {
+		Product product = new Product();
+		product.setId(this.getId());
+		product.setName(this.getName());
+		product.setDescription(this.getDescription());
+		product.setState(this.getState());
+		product.setPrice(this.getPrice());
+		product.setImages(this.getProductImages());
+		return product;
+	}
 }
--- src/main/java/es/model/service/dto/ProductFullDTO.java
+++ src/main/java/es/model/service/dto/ProductFullDTO.java
@@ -1,100 +1,115 @@
 package es.model.service.dto;
 
+import java.util.List;
+
 import es.model.domain.Product;
+import es.model.domain.ProductImage;
 import es.model.domain.State;
 
 public class ProductFullDTO {
-  private Long id;
-  private String name;
+	private Long id;
+	private String name;
 
-  private String location;
+	private String location;
 
-  private String description;
-  private State state;
-  private UserDTO owner;
-  private Double price;
+	private String description;
+	private List<ProductImage> productImages;
+	private State state;
+	private UserDTO owner;
+	private Double price;
 
-  public ProductFullDTO() {}
+	public ProductFullDTO() {
+	}
 
-  public ProductFullDTO(Product product) {
-    this.id = product.getId();
-    this.name = product.getName();
-    this.location = product.getLocation();
-    this.description = product.getDescription();
-    this.state = product.getState();
-    if (product.getOwner() != null) {
-      this.owner = new UserDTO(product.getOwner());
-    }
-    this.price = product.getPrice();
-  }
+	public ProductFullDTO(Product product) {
+		this.id = product.getId();
+		this.name = product.getName();
+		this.location = product.getLocation();
+		this.description = product.getDescription();
+		this.state = product.getState();
+		this.productImages = product.getImages();
+		if (product.getOwner() != null) {
+			this.owner = new UserDTO(product.getOwner());
+		}
+		this.price = product.getPrice();
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getLocation() {
-    return location;
-  }
+	public String getLocation() {
+		return location;
+	}
 
-  public void setLocation(String location) {
-    this.location = location;
-  }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public String getDescription() {
+		return description;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-  public State getState() {
-    return state;
-  }
+	public State getState() {
+		return state;
+	}
 
-  public void setState(State state) {
-    this.state = state;
-  }
+	public void setState(State state) {
+		this.state = state;
+	}
 
-  public UserDTO getOwner() {
-    return owner;
-  }
+	public UserDTO getOwner() {
+		return owner;
+	}
 
-  public void setOwner(UserDTO owner) {
-    this.owner = owner;
-  }
+	public void setOwner(UserDTO owner) {
+		this.owner = owner;
+	}
 
-  public Double getPrice() {
-    return price;
-  }
+	public Double getPrice() {
+		return price;
+	}
 
-  public void setPrice(Double price) {
-    this.price = price;
-  }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-  public Product toProduct() {
-    Product product = new Product();
-    product.setId(this.getId());
-    product.setName(this.getName());
-    product.setLocation(this.getLocation());
-    product.setDescription(this.getDescription());
-    product.setState(this.getState());
-    if (this.getOwner() != null) {
-      product.setOwner(this.getOwner().toUser());
-    }
-    product.setPrice(this.getPrice());
-    return product;
-  }
+	public List<ProductImage> getProductImages() {
+		return productImages;
+	}
+
+	public void setProductImages(List<ProductImage> productImages) {
+		this.productImages = productImages;
+	}
+
+	public Product toProduct() {
+		Product product = new Product();
+		product.setId(this.getId());
+		product.setName(this.getName());
+		product.setLocation(this.getLocation());
+		product.setDescription(this.getDescription());
+		product.setState(this.getState());
+		product.setImages(this.getProductImages());
+		if (this.getOwner() != null) {
+			product.setOwner(this.getOwner().toUser());
+		}
+		product.setPrice(this.getPrice());
+		return product;
+	}
 }
