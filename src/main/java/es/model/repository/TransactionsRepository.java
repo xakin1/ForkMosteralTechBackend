58b9f5bea157f17f2c526c8f98ebc5279c055f5d
xaquín <xaquin.perez1@udc.es>
2023-05-26 19:24:35 +0200

    ajustando formato

--- src/main/java/es/model/repository/TransactionsRepository.java
+++ src/main/java/es/model/repository/TransactionsRepository.java
@@ -13,32 +13,30 @@ import java.util.Optional;
 import es.model.domain.Transactions;
 
 public interface TransactionsRepository
-    extends JpaRepository<Transactions, Long>, JpaSpecificationExecutor<Transactions> {
+		extends JpaRepository<Transactions, Long>, JpaSpecificationExecutor<Transactions> {
 
-  Optional<Transactions> findById(Long pk);
+	Optional<Transactions> findById(Long pk);
 
-  @Query("SELECT t FROM t_transactions t WHERE t.product.id = :productId")
-  Page<Transactions> findByProductId(@Param("productId") Long productId, Pageable pageable);
+	@Query("SELECT t FROM t_transactions t WHERE t.product.id = :productId")
+	Page<Transactions> findByProductId(@Param("productId") Long productId, Pageable pageable);
 
-  Page<Transactions> findByIdIn(List<Long> pk, Pageable pageable);
+	Page<Transactions> findByIdIn(List<Long> pk, Pageable pageable);
 
-  @Query("SELECT t FROM t_transactions t WHERE t.seller.id = :userId")
-  Page<Transactions> findBySellerId(@Param("userId") String userId, Pageable pageable);
+	@Query("SELECT t FROM t_transactions t WHERE t.seller.id = :userId")
+	Page<Transactions> findBySellerId(@Param("userId") String userId, Pageable pageable);
 
-  @Query("SELECT t FROM t_transactions t WHERE t.buyer.id = :userId")
-  Page<Transactions> findByBuyerId(@Param("userId") String userId, Pageable pageable);
+	@Query("SELECT t FROM t_transactions t WHERE t.buyer.id = :userId")
+	Page<Transactions> findByBuyerId(@Param("userId") String userId, Pageable pageable);
 
-  @Query("SELECT COUNT(t) FROM t_transactions t WHERE t.seller.id = :userId")
-  Long countBySellerId(@Param("userId") String userId);
+	@Query("SELECT COUNT(t) FROM t_transactions t WHERE t.seller.id = :userId")
+	Long countBySellerId(@Param("userId") String userId);
 
-  @Query("SELECT COUNT(t) FROM t_transactions t WHERE t.buyer.id = :userId")
-  Long countByBuyerId(@Param("userId") String userId);
+	@Query("SELECT COUNT(t) FROM t_transactions t WHERE t.buyer.id = :userId")
+	Long countByBuyerId(@Param("userId") String userId);
 
-  @Query(
-      "SELECT COUNT(t) FROM t_transactions t WHERE t.buyer.id = :userId OR t.seller.id = :userId")
-  Page<Transactions> findById(String userId, Pageable pageable);
-  
-  @Query(
-	      "SELECT t FROM t_transactions t WHERE t.buyer.id = :userId OR t.seller.id = :userId")
-	  Page<Transactions> findByIdUser(String userId, Pageable pageable);
+	@Query("SELECT COUNT(t) FROM t_transactions t WHERE t.buyer.id = :userId OR t.seller.id = :userId")
+	Page<Transactions> findById(String userId, Pageable pageable);
+
+	@Query("SELECT t FROM t_transactions t WHERE t.buyer.id = :userId OR t.seller.id = :userId")
+	Page<Transactions> findByIdUser(String userId, Pageable pageable);
 }
